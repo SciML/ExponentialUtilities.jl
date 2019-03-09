6128ec9f4c59f2a9396a0f325e540579d1c768db
Yingbo Ma <mayingbo5@gmail.com>
2019-03-09 03:12:26 -0500

    Rough translation

--- src/ExponentialUtilities.jl
+++ src/ExponentialUtilities.jl
@@ -8,10 +8,11 @@ include("phi.jl")
 include("arnoldi.jl")
 include("krylov_phiv.jl")
 include("krylov_phiv_adaptive.jl")
+include("kiops.jl")
 include("StegrWork.jl")
 include("krylov_phiv_error_estimate.jl")
 
 export phi, phi!, KrylovSubspace, arnoldi, arnoldi!, lanczos!, ExpvCache, PhivCache,
-    expv, expv!, phiv, phiv!, expv_timestep, expv_timestep!, phiv_timestep, phiv_timestep!,
+    expv, expv!, phiv, phiv!, kiops, expv_timestep, expv_timestep!, phiv_timestep, phiv_timestep!,
     StegrCache, get_subspace_cache
 end
