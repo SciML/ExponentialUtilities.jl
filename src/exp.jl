44bef93a7e7f8929fa532b3eb26d216805209c52
Mason Protter <mason.protter@icloud.com>
2019-12-08 17:51:06 -0700

    update docstring

--- src/exp.jl
+++ src/exp.jl
@@ -107,9 +107,9 @@ end
 """
     exp(x, vk=Val{10}()) 
 Generic exponential function, working on any `x` for which the functions 
-`LinearAlgebra.opnorm`, `+`, `*`, `^`, and `/` are defined. Use the 
-argument `vk` to adjust the number of terms in the Pade approximants at 
-compile time.
+`LinearAlgebra.opnorm`, `+`, `*`, `^`, and `/` (including addition with 
+UniformScaling objects) are defined. Use the argument `vk` to adjust the 
+number of terms used in the Pade approximants at compile time.
 
 See "The Scaling and Squaring Method for the Matrix Exponential Revisited"
 by Higham, Nicholas J. in 2005 for algorithm details.
