f94346f897863c7f398b50c43a8ce0fe2398fe3b
Elias Jarlebring <jarlebring@gmail.com>
2021-08-17 17:33:36 +0200

    ifs instead of eval

--- src/exp2.jl
+++ src/exp2.jl
@@ -20,7 +20,7 @@ function _exp2!(A; caches=nothing, do_balancing = A isa StridedMatrix)
     rhov=    [0.015; 0.25; 0.95; 2.1; 5.4];
     # Number of memslots needed (beside A)
     memslots=[3   ;    4;    5;   6;   5];
-    for s=1:8
+    for s=1:7 # Only 8 since exp(5.4*2^8)=Inf
         push!(rhov, rhov[end]*2);
         push!(memslots,memslots[end]);
     end
@@ -37,9 +37,33 @@ function _exp2!(A; caches=nothing, do_balancing = A isa StridedMatrix)
 
 
     # Evaluate
-    exp_fun=Symbol("exp_$(i)!")
-    s=:($(exp_fun)($thiscache,$A));
-    X=eval(s)::typeof(A) # Evaluate and maintain type stability
+    if (nA < rhov[1])
+        X=exp_1!(thiscache,A)
+    elseif (nA < rhov[2])
+        X=exp_2!(thiscache,A)
+    elseif (nA < rhov[3])
+        X=exp_3!(thiscache,A)
+    elseif (nA < rhov[4])
+        X=exp_4!(thiscache,A)
+    elseif (nA < rhov[5])
+        X=exp_5!(thiscache,A)
+    elseif (nA < rhov[6])
+        X=exp_6!(thiscache,A)
+    elseif (nA < rhov[7])
+        X=exp_7!(thiscache,A)
+    elseif (nA < rhov[8])
+        X=exp_8!(thiscache,A)
+    elseif (nA < rhov[9])
+        X=exp_9!(thiscache,A)
+    elseif (nA < rhov[10])
+        X=exp_10!(thiscache,A)
+    elseif (nA < rhov[11])
+        X=exp_11!(thiscache,A)
+    elseif (nA < rhov[12])
+        X=exp_12!(thiscache,A)
+    else # X will become very close to Inf
+        X=exp_13!(thiscache,A)
+    end
 
     # Undo the balancing
     if do_balancing
