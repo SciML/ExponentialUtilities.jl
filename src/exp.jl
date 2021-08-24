c557cfa9f4b83e9abea1f12bb8bea8fc03a2d7a0
Elias Jarlebring <jarlebring@gmail.com>
2021-08-24 07:39:58 +0200

    deprecate + docstring

--- src/exp.jl
+++ src/exp.jl
@@ -6,6 +6,7 @@ function alloc_mem(A,method)
     return nothing;
 end
 
+@deprecate _exp! exponential!
 exponential!(A)=exponential!(A,ExpMethodHigham2005(A));
 
 ## The diagonalization based
@@ -19,6 +20,25 @@ struct ExpMethodDiagonalization
     enforce_real::Bool
 end
 ExpMethodDiagonalization()=ExpMethodDiagonalization(true);
+
+
+"""
+    E=exponential!(A,[method [cache]])
+
+Computes the matrix exponential with method specified in `method`. The contents of `A` is modified allowing for less allocations. The `method` parameter specifies the implementation and implementation parameters, e.g. [`ExpMethodNative`](@ref), [`ExpMethodDiagonalization`](@ref), [`ExpMethodGeneric`](@ref), [`ExpMethodHigham2005`](@ref). Memory
+needed can be preallocated and provided in parameter `cache`. The preallocation is done with the command [`alloc_mem`](@ref): `cache=alloc_mem(A,method)`.
+
+Example
+```julia-repl
+julia> A=randn(50,50);
+julia> Acopy=B*2;
+julia> method=ExpMethodHigham2005();
+julia> cache=alloc_mem(A,method); # Main allocation done here
+julia> E1=exponential!(A,method,cache) # Very little allocation here
+julia> E2=exponential!(B,method,cache) # Very little allocation here
+```
+
+"""
 function exponential!(A,method::ExpMethodDiagonalization,cache=nothing)
     F=eigen!(A)
     E=F.vectors*Diagonal(exp.(F.values))/F.vectors
