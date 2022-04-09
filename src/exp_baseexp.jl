52c18dac151d373d5683d944e961ee1d65938795
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-04-09 07:41:09 -0400

    test the GenericSchur fallback

--- src/exp_baseexp.jl
+++ src/exp_baseexp.jl
@@ -34,12 +34,12 @@ function exponential!(A::StridedMatrix{T}, method::ExpMethodHigham2005Base,
 
     fill!(P, zero(T)); fill!(@diagview(P), one(T)) # P = Inn
     
-    if A isa StridedMatrix{<:BlasFloat}
-        ilo, ihi, scale = LAPACK.gebal!('B', A)    # modifies A
-    else
+    #if A isa StridedMatrix{<:BlasFloat}
+    #    ilo, ihi, scale = LAPACK.gebal!('B', A)    # modifies A
+    #else
         A, bal = GenericSchur.balance!(A)
         ilo, ihi, scale = bal.ilo, bal.ihi, bal.D
-    end
+    #end
 
     nA = opnorm(A, 1)
     ## For sufficiently small nA, use lower order Padé-Approximations
