04727fc9b4cc751b53abb49931a8856caad17dbc
Alberto Mercurio <alberto.mercurio96@gmail.com>
2022-10-19 01:27:41 +0200

    Added test

--- test/gpu/gputests.jl
+++ test/gpu/gputests.jl
@@ -1,5 +1,7 @@
-using CUDA: cu
 using LinearAlgebra
+using SparseArrays
+using CUDA
+using CUDA.CUSPARSE
 using ExponentialUtilities: inplace_add!, exponential!, ExpMethodHigham2005
 
 using Random: Xoshiro
@@ -29,4 +31,20 @@ end
             @test collect(eA_d) ≈ eA
         end
     end
+end
+
+@testset "GPU expv and expv_timestep" begin
+    n = 1000
+    A = sprand(ComplexF64, n, n, 10 / n)
+    A = triu(A, 1) + sprand(ComplexF64, n, n, 1 / n)
+    b = rand(ComplexF64, n)
+
+    A_gpu = CuSparseMatrixCSR(A)
+    b_gpu = CuArray(b)
+
+    t = 0.1
+    ts = Array(LinRange(0, 1, 300))
+
+    @test expv(t, A, b) ≈ Array(expv(t, A_gpu, b_gpu))
+    @test expv_timestep(ts, A, b) ≈ Array(expv_timestep(ts, A_gpu, b_gpu))
 end
