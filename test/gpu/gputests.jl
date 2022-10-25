da52559dbb94bda6899f7dda61b8af38ac7dd68b
Chris Rackauckas <accounts@chrisrackauckas.com>
2022-10-25 18:09:33 +0200

    remove set

--- test/gpu/gputests.jl
+++ test/gpu/gputests.jl
@@ -34,21 +34,19 @@ end
     end
 end
 
-@testset "GPU expv and expv_timestep" begin
-    n = 1000
-    A = sprand(ComplexF64, n, n, 10 / n)
-    A = triu(A, 1) + sprand(ComplexF64, n, n, 1 / n)
-    b = rand(ComplexF64, n)
+n = 1000
+A = sprand(ComplexF64, n, n, 10 / n)
+A = triu(A, 1) + sprand(ComplexF64, n, n, 1 / n)
+b = rand(ComplexF64, n)
 
-    A_gpu = CuSparseMatrixCSR(A)
-    b_gpu = CuArray(b)
+A_gpu = CuSparseMatrixCSR(A)
+b_gpu = CuArray(b)
 
-    t = 0.1
-    ts = Array(LinRange(0, 1, 300))
+t = 0.1
+ts = Array(LinRange(0, 1, 300))
 
-    E1 = expv(t, A, b) 
-    E2 = Array(expv(t, A_gpu, b_gpu))
-    @show size(E1), size(E2)
-    E1 ≈ E2
-    @test expv_timestep(ts, A, b) ≈ Array(expv_timestep(ts, A_gpu, b_gpu))
-end
+E1 = expv(t, A, b) 
+E2 = Array(expv(t, A_gpu, b_gpu))
+@show size(E1), size(E2)
+E1 ≈ E2
+@test expv_timestep(ts, A, b) ≈ Array(expv_timestep(ts, A_gpu, b_gpu))
