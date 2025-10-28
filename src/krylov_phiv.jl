858437cbd31329904779f3164b2d586e431da5ef
周唤海 <albus.zhouhh@gmail.com>
2025-10-29 07:45:49 +0800

    Adapt complex dt with complex A and v
    Signed-off-by: 周唤海 <albus.zhouhh@gmail.com>

--- src/krylov_phiv.jl
+++ src/krylov_phiv.jl
@@ -126,7 +126,8 @@ function expv!(w::AbstractVector{Complex{Tw}}, t::Complex{Tt}, Ks::KrylovSubspac
         expHe = @view(expH[:, 1])
     end
     # `ArrayInterfaceCore.restructure` will convert the `expHe` to the target matrix type that can interact with `V`.
-    lmul!(beta, mul!(w, @view(V[:, 1:m]), compatible_multiplicative_operand(V, expHe))) # exp(A) ≈ norm(b) * V * exp(H)e
+    # lmul!(beta, mul!(w, @view(V[:, 1:m]), compatible_multiplicative_operand(V, expHe))) # exp(A) ≈ norm(b) * V * exp(H)e
+    lmul!(beta, mul!(w, @view(V[:, 1:m]), Adapt.adapt(parameterless_type(w), expHe)))
 end
 
 function ExponentialUtilities.expv!(w::GPUArraysCore.AbstractGPUVector{Tw},
